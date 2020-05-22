f05966bc9bc66ef811f3c19b38f7460025009284
Daniel Tchangang <daniel.tchangang@gmail.com>
2020-05-22 09:45:36 +0200

    Remove activeTab permission

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -12,36 +12,6 @@ const {
 
 export let cardsByCardUrl: Record<string, ICard> = {};
 
-chrome.runtime.onMessage.addListener(function (
-  request: any,
-  sender: any,
-  sendResponse: any,
-) {
-  console.log('we have babael 8');
-  console.log(
-    sender.tab
-      ? 'from a content script:' + sender.tab.url
-      : 'from the extension ',
-  );
-  console.log('new request came', request);
-  switch (request.action) {
-    case REFRESH_CARD_ID:
-      actionsManager.initializeActions();
-      break;
-    case SAVE_DEPENDENCIES_ID:
-      sendResponse(dependencyManager.getDependencies());
-      break;
-    case RENDER_DEPENDENCIES_ID:
-      const { dependencies } = request.data;
-      actionsManager.initializeActions();
-      dependencyManager.renderDependencies(dependencies);
-      break;
-    default:
-      break;
-  }
-});
-
-
 if (document.readyState == 'loading') {
   // loading yet, wait for the event
   document.addEventListener('DOMContentLoaded',  function () {
