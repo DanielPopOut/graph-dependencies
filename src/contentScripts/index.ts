769fc1246b08c945c3707ed909182309d265e484
Daniel Tchangang <daniel.tchangang@gmail.com>
2020-05-23 19:44:23 +0200

    Update graphDependency for chrome store publication and zip store

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -2,14 +2,8 @@ import { actionsManager } from './actions/actionManager';
 
 declare var chrome: any;
 
-export let cardsByCardUrl: Record<string, ICard> = {};
 
-if (document.readyState == 'loading') {
-  // loading yet, wait for the event
-  document.addEventListener('DOMContentLoaded', function () {
-    actionsManager.addRefreshActionsButton();
-  });
-} else {
-  // DOM is ready!
+console.log('[GRAPH DEP EXTENSION LOADING]');
+window.addEventListener('load', function () {
   actionsManager.addRefreshActionsButton();
-}
+});
